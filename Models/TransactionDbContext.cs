8bdd474aa3acbc2ab89abc2feaf26387e752ce8a
tenidaniels <52575436+tenidaniels@users.noreply.github.com>
2022-05-05 17:43:41 +0100

    Add files via upload
    used JQueryAjax

---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Banking.Models
+{
+    public class TransactionDbContext:DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        { }
+
+        public DbSet<TransactionModel> Transactions { get; set; }
+    }
+}
---
+++ Models/TransactionModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Banking.Models
+{
+    public class TransactionModel
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [MaxLength(12)]
+        [Required(ErrorMessage ="This field is required.")]
+        [DisplayName("Account Number")]
+        [Column(TypeName = "nvarchar(12)")]
+        public string AccountNumber { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
+        [DisplayName("Beneficiary Name")]
+        [Column(TypeName = "nvarchar(100)")]
+        public string BeneficiaryName { get; set; }
+
+        [Required(ErrorMessage = "This field is required.")]
+        [DisplayName("BankName")]
+        [Column(TypeName = "nvarchar(100)")]
+        public string BankName { get; set; }
+
+
+        [MaxLength(12)]
+        [Required(ErrorMessage = "This field is required.")]
+        [DisplayName("SWIFT Code")]
+        [Column(TypeName = "nvarchar(11)")]
+        public string SWIFTCode { get; set; }
+
+        [DisplayName("Amount")]
+        [Required(ErrorMessage ="This field is required.")]
+        public int Amount { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:MM/dd/yyyy}")]
+        public DateTime Date { get; set; }
+    
+}
+}
